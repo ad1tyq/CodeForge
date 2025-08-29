d692a3768b0479caf3c47f79106bc51256299e6e
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-29 23:10:40 +0530

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import CustomLink from "./CustomLink";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+function Navbar() {
+    const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 30) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={`${isScrolled ? "bg-black/20 backdrop-blur-lg shadow-sm" : "bg-emerald-900"} w-full h-[5rem] flex items-center justify-between px-10 sticky top-0 z-20 transition-all duration-300`}>
+            <CustomLink href="/">
+                <Image src="/assets/leaf.png" alt="icon" width={44} height={44}/>
+                <span className="font-semibold text-[1.25rem] pr-4">CodeForge</span>
+            </CustomLink>
+            <div className="flex text-md font-semibold gap-10">
+                <CustomLink href="/#about">
+                    <span className="py-2 pr-2">About</span>
+                </CustomLink>
+                <CustomLink href="/dashboard">
+                    <span className="py-2 pr-2 pl-3">Dashboard</span>
+                </CustomLink>
+            </div>
+        </div>
+    );
+}
+export default Navbar;
