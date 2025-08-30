c9e496f92e230c3e12b7110ff3ad6ce56bd0b63e
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-30 07:20:27 +0530

    fixing deployment issues: take 5

--- src/components/gamecomp/ui/form.tsx
+++ src/components/gamecomp/ui/form.tsx
@@ -90,7 +90,7 @@ function FormItem({ className, ...props }: React.ComponentProps<"div">) {
 function FormLabel({
   className,
   ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+}: React.ComponentProps<typeof Label>) { // Change from LabelPrimitive.Root to Label
   const { error, formItemId } = useFormField();
 
   return (
